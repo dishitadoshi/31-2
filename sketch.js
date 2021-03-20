c6d1f6289d62b153745e966c00eddad9370b3acc
dishitadoshi <71959138+dishitadoshi@users.noreply.github.com>
2021-03-20 17:05:13 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@ var engine, world;
 var drop = [];
 var MaxDrop =100;
  var umbrella;
+var thunder;
 function preload(){
     
 }
@@ -18,7 +19,19 @@ function setup(){
     for(var i=0; i<MaxDrop; i++){
         drop.push(new Drop(random(0,400), random(0,400)));
     }
-   
+   rand = Math.round(random(1,4));
+ if(frameCount%80===0){
+  thuderCreatedFrame=frameCount;
+  thunder = createSprite(random(10,370), random(10,30),10,10);
+  switch(rand){
+   case 1:thunder.addImage(thunder1);
+    break;
+   case 2: thunder.addImage(thunder2);
+    break;
+   default: break;
+  }
+  thunder.scale = random(0.3,0.6);
+ }
 }
 
 function draw(){
